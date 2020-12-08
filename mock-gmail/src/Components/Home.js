d3db86f978f5b11bb65078f49f696fe60d7a8588
moldux-dartus <moldux.dartus+github@gmail.com>
2020-12-08 13:55:43 -0600

    Message draft popup now works

--- mock-gmail/src/Components/Home.js
+++ mock-gmail/src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Email from './Email'
+import Popup from 'reactjs-popup';
 
 const Home = ({ emails }) => {
   const emails2 = emails
@@ -7,7 +8,23 @@ const Home = ({ emails }) => {
       <>
       <span>INBOX</span>
         <Email data={emails2}/>
-        
+        <div />
+        <div />
+        <Popup trigger={<button>Draft New Email</button>} position="right bottom">
+              <div>
+                <label for="subject">Subject</label>
+                <input id='subject'></input>
+                <div />
+                <label for="recipient">Recipient</label>
+                <input id='recipient'></input>
+                <div />
+                <label for="mb">Message Body</label>
+                <div />
+                <textarea id="mb" rows="4" cols="50"></textarea>
+                <div />
+                <button>Send</button>
+              </div>
+            </Popup>
       </>
     )
 }
